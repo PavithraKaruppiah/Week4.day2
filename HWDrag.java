70f89e571f96a52a45880cc5acc0c7608f085b7d
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-05-04 01:48:05 +0300

    Add files via upload

---
+++ HWDrag.java
@@ -0,0 +1,27 @@
+package week4.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HWDrag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/drag.html");
+		driver.manage().window().maximize();
+		
+		WebElement drag = driver.findElement(By.id("draggable"));
+		Actions builder = new Actions(driver);
+		//drag
+		builder.dragAndDropBy(drag, 150, 100).perform();
+		
+}
+
+}
