3330cc905705df450cf286fb662437ab366bf852
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-05-04 01:48:18 +0300

    Add files via upload

---
+++ HWDrop.java
@@ -0,0 +1,27 @@
+package week4.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HWDrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/drop.html");
+		driver.manage().window().maximize();
+		
+		WebElement drag =driver.findElement(By.id("draggable"));
+		WebElement drop =driver.findElement(By.id("droppable"));
+		
+		Actions builder = new Actions(driver);
+		builder.dragAndDrop(drag, drop).perform();
+
+	}
+
+}
